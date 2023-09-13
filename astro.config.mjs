5f0c3be5e85307fb8b35420a5d24abe96c84d1ba
Pocee <48737111+Pocee@users.noreply.github.com>
2023-09-13 18:42:43 +0200

    Try2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pocee.github.io/',
-  base: '/PoceBlog',
+  base: '/PoceBlog/',
   integrations: [tailwind()]
 });
