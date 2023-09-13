b56d4627002ca4a33386d76d2a356b25df83ab6e
Pocee <48737111+Pocee@users.noreply.github.com>
2023-09-13 18:09:38 +0200

    Trying to add .github 1/x

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pocee.github.io',
-  base: '/PoceBlog',
   integrations: [tailwind()]
 });
